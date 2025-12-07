8847e6b0204eca3963bd0bb82f90fcc8a96057aa
Myat Thiha Naing <myatthihanaing.mthn@gmail.com>
2025-12-07 23:09:56 +0700

    fix args in entrypoint.js

--- 06-authoring-actions/container-actions/node-container-action/entrypoint.js
+++ 06-authoring-actions/container-actions/node-container-action/entrypoint.js
@@ -1,3 +1,3 @@
 
-const greetingName = process.env.INPUT_WHO_TO_GREET || 'World';
+const greetingName = process.argv[2] || 'Default World';
 console.log(`Hello ${greetingName}! from a Node.js container action!`);
